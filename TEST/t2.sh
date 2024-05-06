ee7e7e11d6fbce047846e65648153a672f0a0e7e
Richard Durbin <rd109@cam.ac.uk>
2024-05-07 00:52:31 +0100

    test indexing

---
+++ TEST/t2.sh
@@ -0,0 +1,9 @@
+../ONEview -b t2.seq > ZZ_2.1seq
+echo "TEST2 object S 0"
+../ONEview -h -i S 0 ZZ_2.1seq
+echo "TEST2 object S 1"
+../ONEview -h -i S 1 ZZ_2.1seq
+echo "TEST2 group s 0"
+../ONEview -h -i s 0 ZZ_2.1seq
+echo "TEST2 group s 1"
+../ONEview -h -i s 1 ZZ_2.1seq
