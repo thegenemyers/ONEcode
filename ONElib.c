6b74058186b70d1f0cd78c4f24381ac2f9c682c6
Richard Durbin <rd109@cam.ac.uk>
2024-09-02 20:20:44 +0100

    fix ltfRead() for small -ve integers

--- ONElib.c
+++ ONElib.c
@@ -7,7 +7,7 @@
  *  Copyright (C) Richard Durbin, Cambridge University and Eugene Myers 2019-
  *
  * HISTORY:
- * Last edited: Aug 22 13:57 2024 (rd109)
+ * Last edited: Sep  2 20:18 2024 (rd109)
  * * May  1 00:23 2024 (rd109): moved to OneInfo->index and multiple objects/groups
  * * Apr 16 18:59 2024 (rd109): major change to object and group indexing: 0 is start of data
  * * Mar 11 02:49 2024 (rd109): fixed group bug found by Gene
@@ -3764,7 +3764,11 @@ static inline I64 ltfRead (FILE *f)
 
   u[0] = getc (f) ;
   if (u[0] & 0x40)
-    val = (I64) (u[0] & 0x3f) ;
+    { if (u[0] & 0x80) // negative
+	val = (char) u[0] ;
+      else
+	val = (I64) (u[0] & 0x3f) ;
+    }
     // { intGet (u, &val) ;
     //   printf ("read %d n 1 u %02x\n", (int)val, u[0]) ;
     // }
@@ -4104,7 +4108,7 @@ int main (int argc, char *argv[])
 
 /***********************************************************************************
  *
- *    UTILITIES: memory allocation, file opening, timer
+ *    UTILITIES: die(), memory allocation
  *    adapted from Richard's utilities
  *
  **********************************************************************************/
