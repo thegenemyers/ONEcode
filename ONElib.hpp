c285a8f115b6e354e1e5c39d11702727419380e1
Richard Durbin <rd109@cam.ac.uk>
2024-09-20 18:36:11 +0100

    added write functionality and cleaned up interface

--- ONElib.hpp
+++ ONElib.hpp
@@ -38,74 +38,101 @@ class ONEschema
 class ONEfile
 {
  private:
-  C_1F::OneFile *vf ;
+  C_1F::OneFile *of ;
   
  public:
   ONEfile (const string &path) // just to open an existing file for reading with one thread
-    { vf = NULL ;
-      vf = C_1F::oneFileOpenRead (path.c_str(), 0, 0, 1) ;
-      if (vf == NULL) { throw runtime_error("failed to open ONEfile") ; } 
+    { of = NULL ;
+      of = C_1F::oneFileOpenRead (path.c_str(), 0, 0, 1) ;
+      if (of == NULL) { throw runtime_error("failed to open ONEfile") ; } 
     }
   ONEfile (const string &path, int nthreads) // open an existing file for reading with nthreads
-    { vf = NULL ;
-      vf = C_1F::oneFileOpenRead (path.c_str(), 0, 0, nthreads) ;
-      if (vf == NULL) { throw runtime_error("failed to open ONEfile") ; } 
+    { of = NULL ;
+      of = C_1F::oneFileOpenRead (path.c_str(), 0, 0, nthreads) ;
+      if (of == NULL) { throw runtime_error("failed to open ONEfile") ; } 
     }
   ONEfile (const string &path, const string &mode, const ONEschema &schema, const string &type, int nthreads) // full version
-    { vf = NULL ;
+    { of = NULL ;
       const char* tc = (type.size() > 0) ? type.c_str() : 0 ;
       if (mode[0] == 'r')
-	vf = C_1F::oneFileOpenRead (path.c_str(), schema.os, tc, nthreads) ;
+	of = C_1F::oneFileOpenRead (path.c_str(), schema.os, tc, nthreads) ;
       else if (mode[0] == 'w' && mode[1] == 'b')
-	vf = C_1F::oneFileOpenWriteNew (path.c_str(), schema.os, tc, true, nthreads) ;
+	of = C_1F::oneFileOpenWriteNew (path.c_str(), schema.os, tc, true, nthreads) ;
       else if (mode[0] == 'w' && mode.size() == 1)
-	vf = C_1F::oneFileOpenWriteNew (path.c_str(), schema.os, tc, false, nthreads) ;
-      if (vf == NULL) { throw runtime_error("failed to open ONEfile") ; } 
+	of = C_1F::oneFileOpenWriteNew (path.c_str(), schema.os, tc, false, nthreads) ;
+      if (of == NULL) { throw runtime_error("failed to open ONEfile") ; } 
     }
   ONEfile (const string &path, const string &mode, ONEfile &from, int nthreads)
-    { vf = NULL ;
+    { of = NULL ;
       if (mode[0] == 'w' && mode[1] == 'b')
-	vf = C_1F::oneFileOpenWriteFrom (path.c_str(), from.vf, true, nthreads) ;
+	of = C_1F::oneFileOpenWriteFrom (path.c_str(), from.of, true, nthreads) ;
       else if (mode[0] == 'w' && mode.size() == 1)
-	vf = C_1F::oneFileOpenWriteFrom (path.c_str(), from.vf, false, nthreads) ;
-      if (vf == NULL) { throw runtime_error("failed to open ONEfile") ; } 
+	of = C_1F::oneFileOpenWriteFrom (path.c_str(), from.of, false, nthreads) ;
+      if (of == NULL) { throw runtime_error("failed to open ONEfile") ; } 
     }
-  ~ONEfile () { C_1F::oneFileClose (vf) ; }
+  ~ONEfile () { C_1F::oneFileClose (of) ; }
 
   bool      checkSchemaText (const string &text)
-    { return C_1F::oneFileCheckSchemaText (vf, text.c_str()) ; }
+    { return C_1F::oneFileCheckSchemaText (of, text.c_str()) ; }
 
-  char      readLine() { return C_1F::oneReadLine (vf) ; }
+  char      readLine() { return C_1F::oneReadLine (of) ; }
 
-  int64_t   length()
-    { return ((vf)->field[((vf)->info[(int)(vf)->lineType]->listField)].len & 0xffffffffffffffll) ; }
-  int64_t   getInt(int x) { return vf->field[x].i ; }
-  void      setInt(int x, int64_t val) { vf->field[x].i = val ; }
-  double    getReal(int x) { return vf->field[x].r ; }
-  void      setReal(int x, double val) { vf->field[x].r = val ; }
-  char      getChar(int x) { return vf->field[x].c ; }
-  void      setChar(int x, char val) { vf->field[x].c = val ; }
-  int64_t*  getIntList() { return (int64_t*) C_1F::_oneList(vf) ; }
-  double*   getRealList() { return (double*) C_1F::_oneList(vf) ; }
-  char*     getDNAchar() { return (char*) _oneList(vf) ; }
-  uint8_t*  getDNA2bit () { return (uint8_t*) C_1F::_oneCompressedList(vf) ; }
-  string    getString() { return (char *) C_1F::_oneList(vf) ; }
-  char*     nextString(char* s) { return s + strlen(s) + 1 ; }
+  int64_t   listLength()
+    { return ((of)->field[((of)->info[(int)(of)->lineType]->listField)].len & 0xffffffffffffffll) ; }
+  int64_t   getInt(int x) { return of->field[x].i ; }
+  double    getReal(int x) { return of->field[x].r ; }
+  char      getChar(int x) { return of->field[x].c ; }
+  int64_t*  getIntList() { return (int64_t*) C_1F::_oneList(of) ; }
+  double*   getRealList() { return (double*) C_1F::_oneList(of) ; }
+  char*     getDNAchar() { return (char*) _oneList(of) ; }
+  uint8_t*  getDNA2bit () { return (uint8_t*) C_1F::_oneCompressedList(of) ; }
+  string    getString() { return (char *) C_1F::_oneList(of) ; }
+  vector<string> getStringList ()
+  { char *s = (char*) C_1F::_oneList(of) ;
+      int size = listLength() ;
+      vector<string> vs(size) ;
+      int i ; for (i = 0 ; i < size ; ++i) { vs[i] = strdup(s) ; s += strlen(s) + 1 ; }
+      return vs ;
+    }
 
-  char*     getComment() { return C_1F::oneReadComment(vf) ; }
+  char*     getComment() { return C_1F::oneReadComment(of) ; }
 
-  bool      gotoObject(char lineType, int64_t i) { return C_1F::oneGoto (vf, lineType, i) ; }
+  bool      gotoObject(char lineType, int64_t i) { return C_1F::oneGoto (of, lineType, i) ; }
+
+  // routines to write
+
+  void      setInt(int x, int64_t val) { of->field[x].i = val ; }
+  void      setReal(int x, double val) { of->field[x].r = val ; }
+  void      setChar(int x, char val) { of->field[x].c = val ; }
+  void      writeLine(char lineType) { C_1F::oneWriteLine(of, lineType, 0, 0) ; }
+  void      writeLine(char lineType, int64_t listLen, void *listBuf)
+    { C_1F::oneWriteLine(of, lineType, listLen, listBuf) ; }
+  void      writeLine(char lineType, string s)
+  { C_1F::oneWriteLine(of, lineType, s.length(), (void*) s.c_str()) ; }
+  void      writeLine(char lineType, const vector<string>& slist)
+    { int i, size = slist.size() ;
+      int totLen = 0 ; for (i = 0 ; i < size ; ++i) totLen += slist[i].length() ;
+      char *s, *s0 = new char[totLen+size] ;
+      for (i = 0, s = s0 ; i < size ; ++i, s += slist[i].length() + 1)
+	strcpy (s, slist[i].c_str()) ;
+      C_1F::oneWriteLine(of, lineType, size, s0) ;
+    }
+  void      writeLineDNA2bit (char lineType, int64_t dnaLen, uint8_t *dnaBuf)
+  { C_1F::oneWriteLineDNA2bit (of, lineType, dnaLen, dnaBuf) ; }
+  
+  void      writeComment (string s) { C_1F::oneWriteComment(of, (char*)"%s", s.c_str()) ; }
+
 
   // some extra functions to hide readable class attributes
 
-  char      lineType() { return vf->lineType ; }
-  int64_t   lineNumber() { return vf->line ; }
-  int64_t   givenCount(char lineType) { return vf->info[lineType]->given.count ; }
-  int64_t   givenMax(char lineType) { return vf->info[lineType]->given.max ; }
-  int64_t   givenTotal(char lineType) { return vf->info[lineType]->given.total ; }
-  int64_t   currentCount(char lineType) { return vf->info[lineType]->accum.count ; }
-  int64_t   currentMax(char lineType) { return vf->info[lineType]->accum.max ; }
-  int64_t   currentTotal(char lineType) { return vf->info[lineType]->accum.total ; }
+  char      lineType() { return of->lineType ; }
+  int64_t   lineNumber() { return of->line ; }
+  int64_t   givenCount(char lineType) { return of->info[lineType]->given.count ; }
+  int64_t   givenMax(char lineType) { return of->info[lineType]->given.max ; }
+  int64_t   givenTotal(char lineType) { return of->info[lineType]->given.total ; }
+  int64_t   currentCount(char lineType) { return of->info[lineType]->accum.count ; }
+  int64_t   currentMax(char lineType) { return of->info[lineType]->accum.max ; }
+  int64_t   currentTotal(char lineType) { return of->info[lineType]->accum.total ; }
 } ;
 
 #ifdef TEST_HEADER
@@ -134,7 +161,7 @@ int main (int argc, char *argv[])
   
   while (of.readLine())
     if (of.lineType() == 'S')
-      cout << "sequence length " << of.length() << "\n" ;
+      cout << "sequence length " << of.listLength() << "\n" ;
 }
 
 #endif
